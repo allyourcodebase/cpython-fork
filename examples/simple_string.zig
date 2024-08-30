eed878f4ab48a2aa1cea93d6640c1f1f0a04b355
John Murray <5672686+JRMurr@users.noreply.github.com>
2024-08-29 22:46:06 -0400

    cleanup example

--- examples/simple_string.zig
+++ examples/simple_string.zig
@@ -32,13 +32,6 @@ pub fn Initialize(
     py.externs.PyConfig_InitIsolatedConfig(&config);
     defer py.externs.PyConfig_Clear(&config);
 
-    // py.externs.Py_SetProgramName("test");
-
-    // mute some silly errors that probably do infact matter
-    // config.pathconfig_warnings = 0;
-
-    // config.program_name = @constCast(try utf8ToUtf32Z("test", allocator));
-
     var status = py.externs.PyConfig_SetBytesString(
         &config,
         &config.program_name,
@@ -48,35 +41,6 @@ pub fn Initialize(
     if (py.externs.PyStatus_Exception(status)) {
         py.externs.Py_ExitStatusException(status);
     }
-
-    // const lib_path = try allocator.dupeZ(u8, py.constants.LIB_PATH);
-
-    // status = py.externs.PyConfig_SetBytesString(
-    //     &config,
-    //     &config.home,
-    //     lib_path,
-    // );
-
-    // if (py.externs.PyStatus_Exception(status)) {
-    //     py.externs.Py_ExitStatusException(status);
-    // }
-
-    // status = py.externs.PyConfig_SetBytesString(
-    //     &config,
-    //     &config.pythonpath_env,
-    //     lib_path,
-    // );
-
-    // if (py.externs.PyStatus_Exception(status)) {
-    //     py.externs.Py_ExitStatusException(status);
-    // }
-
-    // status = py.externs.PyConfig_Read(&config);
-    // if (py.externs.PyStatus_Exception(status)) {
-    //     py.externs.Py_ExitStatusException(status);
-    // }
-
-    std.debug.print("py.constants.LIB_PATH: {s}\n", .{py.constants.LIB_PATH});
 
     const utf32_path = try utf8ToUtf32Z(
         py.constants.LIB_PATH,
@@ -93,18 +57,7 @@ pub fn Initialize(
         py.externs.Py_ExitStatusException(status);
     }
 
-    std.debug.print("config: {}\n", .{config});
     try init_from_config(&config);
-    // status = py.externs.Py_InitializeFromConfig(&config);
-
-    // // std.debug.print("END STATUS: {}\n", .{&status});
-
-    // if (py.externs.PyStatus_Exception(status)) {
-    //     py.externs.Py_ExitStatusException(status);
-    // }
-    // needs to be a pointer discard because the stack protector gets overrun?
-    // _ = &status;
-    std.debug.print("END\n", .{});
 
     py.externs.PyConfig_Clear(&config);
 }
@@ -115,12 +68,8 @@ pub fn main() !void {
     try Initialize(allocator);
     defer py.externs.Py_Finalize();
 
-    std.debug.print("AFTER INIT\n", .{});
-
     py.externs.PyRun_SimpleString(
         \\from time import time,ctime
         \\print('Today is', ctime(time()))
     );
-
-    // py.externs.PyRun_SimpleString("print('hi')");
 }
